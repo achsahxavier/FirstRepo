1bb6dfc6cd200da1a7bbdf3483977d2fb92d0570
achsahxavier <achsahxavier1001@gmail.com>
2023-01-17 00:00:17 +0530

    added pages

---
+++ index.js
@@ -0,0 +1,74 @@
+//Declaring 
+    // function msg() {
+    //     return "Hello World";
+    // }
+
+    // //Calling
+    // //let a = msg()
+    // console.log(msg())
+
+    // function addNumbers(a=5, b=6) {
+    //     console.log(a+b)
+    // }
+
+    // addNumbers(b=8)
+
+    // function addNumbers(a,b)
+    // {
+    //     console.log(a)
+    //     console.log(b)
+    //     console.log(a+b)
+    // }
+    
+    // addNumbers(b=8, a=0)
+
+    // var msg = function() {
+    //     return "hiii....."
+    // }
+
+    // console.log(msg())
+
+//     const student = {
+//         name : "tarun",
+//         age : 20,
+//         email : "tarun@gmail.com",
+//         address : {
+//         city : "Jalandhar",
+//         state : "Punjab",
+//         country : "India"
+//     }
+// }
+
+//     student.phone = 5656757890
+//     console.log(student.address.state)
+
+// function add(a,b) {
+//     console.log(a+b);
+// }
+
+// add(3,4,5)
+
+// var m = 5;
+// while(true) {
+//     let n =9 ;
+//     if(m>n)
+//     return true;
+//     else
+//     return false;
+// }
+
+// let i=10;
+// console.log(i++);
+// console.log(--i);
+
+let a=2;
+let b=3;
+
+function swap() {
+    temp = a;
+    a=b;
+    b=temp;
+}
+
+console.log(a);
+console.log(b);
---
+++ script.js
@@ -0,0 +1,12 @@
+// var a=15;
+// var b=15;
+// var c = b/a;
+// console.log(a==b);
+
+function hello() {
+    console.log("hi");
+}
+
+var a = hello();
+console.log(a);
+console.log(typeof a)
