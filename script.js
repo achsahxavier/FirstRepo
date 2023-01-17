1d6d86b3a760768308b3a17583acf32beb81af8e
achsahxavier <achsahxavier1001@gmail.com>
2023-01-18 01:25:52 +0530

    changes made

--- script.js
+++ script.js
@@ -3,10 +3,18 @@
 // var c = b/a;
 // console.log(a==b);
 
-function hello() {
-    console.log("hi");
-}
+// function hello() {
+//     console.log("hi");
+// }
 
-var a = hello();
-console.log(a);
-console.log(typeof a)
+// var a = hello();
+// console.log(a);
+// console.log(typeof a)
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10];
+//  const odds = numbers.filter((num) => num % 2 ===1);
+//  console.log(odds);
+
+const prices = [1000,2000,null,3000,5000,"Thousand",6000];
+ const value = prices.filter((num) => num > 2000);
+ console.log(value);
